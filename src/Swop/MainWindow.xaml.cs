dfd80574c6b1b4c7a9a0ee0f3872d43e75d3d7aa
titushm <titusmeader@btinternet.com>
2025-01-29 22:10:31 +0000

    Forgot gitignore, added rest of initial code

---
+++ src/Swop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Swop;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e){
+        Dictionary<string, string> apps = Workshop.GetWorkshopAppIDs();
+        foreach (KeyValuePair<string, string> item in apps) {
+            TabItem tabItem = new TabItem();
+            tabItem.Header = item.Value;
+            tabItem.Tag = item.Key;
+            AppsTabControl.Items.Add(tabItem);
+
+        }
+
+    }
+}
---
+++ src/Swop/ModsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Swop
+{
+    /// <summary>
+    /// Interaction logic for ModsPage.xaml
+    /// </summary>
+    public partial class ModsPage
+    {
+        public ModsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveProfileButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+            
+        }
+
+        private void ImportProfileButton_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+        }
+    }
+}
