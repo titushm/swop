1081e4f7f166d99530f66e1d7bac2b497e6d0fa5
titushm <titusmeader@btinternet.com>
2025-01-30 21:57:00 +0000

    - Added loading pages and error pages - better app icon - completed profile system - 80% completed mods system. - Caches the mod names and ids to save time each launch.

---
+++ src/Swop/ContentErrorPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Swop;
+
+public partial class ContentErrorPage : Page{
+    public ContentErrorPage(){
+        InitializeComponent();
+    }
+}
---
+++ src/Swop/LoadingContentPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Swop;
+
+public partial class LoadingContentPage : Page{
+    public LoadingContentPage(){
+        InitializeComponent();
+    }
+}
